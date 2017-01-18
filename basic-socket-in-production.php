60707d35f760f3255d74b06188cc8c8b7e5ebb36
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-01-18 18:30:35 +0700

    basic toggle when click button to demo

--- basic-socket-in-production.php
+++ basic-socket-in-production.php
@@ -1,31 +1,67 @@
 <?php
-$rooms = ['room A', 'room B', 'room C', 'room D'];
+$rooms = [
+  [
+    'id' => 'roomA',
+    'name' => 'room A'
+  ],
+  [
+    'id' => 'roomB',
+    'name' => 'room B'
+  ],
+  [
+    'id' => 'roomC',
+    'name' => 'room C'
+  ],
+  [
+    'id' => 'roomD',
+    'name' => 'room D'
+  ],
+  [
+    'id' => 'roomE',
+    'name' => 'room E'
+  ]        
+];
 ?>
 <!doctype html>
 <html>
   <head>
     <title>List room</title>
   </head>
+  <style>
+    .booked {
+      background-color: #00a65a;
+    }
+  </style>
   <body>
     <ul id="rooms">
       <?php foreach($rooms as $room): ?>
-        <li><span><?php echo $room; ?></span><button>book</button></li>
+        <li id="<?php echo $room['id']; ?>"><span><?php echo $room['name']; ?></span><button room-id="<?php echo $room['id']; ?>">book</button></li>
       <?php endforeach; ?>
     </ul>
     <script src="http://reservation.tinker.press/socket.io/socket.io.js"></script>
-    <script src="http://code.jquery.com/jquery-1.11.1.js"></script>
     <script>
       var socket = io('reservation.tinker.press');
-      $('form').submit(function(){
-        socket.emit('chat message', $('#m').val());
-        // socket.broadcast.emit('chat message', $('#m').val());
-        $('#m').val('');
-        return false;
+
+      let ul = document.querySelector('#rooms');
+      ul.addEventListener('click', function(e){
+        // console.log(e);
+        let btn = e.target;
+        let roomId = btn.getAttribute('room-id');
+
+        socket.emit('chat message', roomId);
       });
 
-      socket.on('chat message', function(msg){
-        console.log(msg);
-        $('#messages').append($('<li>').text(msg));
+      socket.on('chat message', function(roomId){
+        console.log('room id:', roomId);
+        let clickedElement = document.querySelector('#' + roomId);
+        let btn = clickedElement.childNodes[1];
+        if(clickedElement.classList.contains('booked')){
+          clickedElement.classList = '';
+          btn.innerText = 'book';
+        }else{
+          clickedElement.classList = 'booked';
+          btn.innerText = 'unbook';
+        }
       });
     </script>
   </body>
