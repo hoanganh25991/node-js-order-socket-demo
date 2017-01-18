c6f294999622ee8fdb53296b13871caab727277c
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-01-18 18:07:02 +0700

    fake data

--- basic-socket-in-production.php
+++ basic-socket-in-production.php
@@ -1,10 +1,17 @@
+<?php
+$rooms = ['room A', 'room B', 'room C', 'room D'];
+?>
 <!doctype html>
 <html>
   <head>
     <title>List room</title>
   </head>
   <body>
-    <ul id="rooms"></ul>
+    <ul id="rooms">
+      <?php foreach($rooms as $room): ?>
+        <li><span><?php echo $room; ?></span><button>book</button></li>
+      <?php endforeach; ?>
+    </ul>
     <script src="http://reservation.tinker.press/socket.io/socket.io.js"></script>
     <script src="http://code.jquery.com/jquery-1.11.1.js"></script>
     <script>
